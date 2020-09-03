fc1af2551a01f1284855b4efbe83eb5917c743f6
LoebKalin <loeb.kalin79@gmail.com>
2020-09-03 09:27:16 +0700

    update register

--- server/middleware/validInfo.js
+++ server/middleware/validInfo.js
@@ -1,12 +1,12 @@
 module.exports = (req, res, next) => {
-  const { email, name, password } = req.body;
+  const { email, password } = req.body;
 
   function validEmail(userEmail) {
     return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(userEmail);
   }
 
   if (req.path === "/register") {
-    if (![email, name, password].every(Boolean)) {
+    if (![email, password].every(Boolean)) {
       return res.status(401).json("Missing Credentials");
     } else if (!validEmail(email)) {
       return res.status(401).json("Invalid Email");
