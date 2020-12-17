119efa501a567cb91aa7273b9f773c65fd1d3eea
LoebKalin <loeb.kalin79@gmail.com>
2020-12-17 10:58:36 +0700

    testing

---
+++ server/utils/check_validwallet.js
@@ -0,0 +1,40 @@
+const axios = require("axios");
+require("dotenv").config();
+const pool = require("../../db");
+
+
+const checkBalance = (usr)=>{
+    const balance;
+    try{
+    const checkWallet = await pool.query(
+        "SELECT ids FROM users_email WHERE id = $1",
+        [usr]
+    );
+
+    const userPortfolio = {
+        id: checkWallet.rows[0].ids,
+        apikey: process.env.API_KEYs,
+        apisec: process.env.API_SEC,
+    };
+    if (checkWallet.rows[0].ids === null) {
+        res.send("Please get wallet first!");
+    } else {
+        axios
+            .post(
+                "https://testnet-api.selendra.com/apis/v1/portforlio-by-api",
+                userPortfolio
+            )
+            .then((r) => {
+                balance=JSON.parse(JSON.stringify(r.data.body.data.balance));
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    }
+} catch (err) {
+    console.error(err);
+    res.status(500).send("Server error!");
+}
+}
+
+module.exports={checkBalance};
