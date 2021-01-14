7758d7e90d34a126031f3ac44fa10c8bece92fff
LoebKalin <loeb.kalin79@gmail.com>
2021-01-14 13:47:46 +0700

    send sms

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-cd server && npm install express pg bcrypt cors dotenv jsonwebtoken moment body-parser aws-sdk express-fileupload morgan multer lodash axios && npm run server
+cd server && npm install express pg bcrypt cors dotenv jsonwebtoken moment body-parser aws-sdk express-fileupload morgan multer lodash axios twilio && npm run server
 
 #install node module
-# npm install express pg bcrypt cors dotenv jsonwebtoken moment body-parser aws-sdk express-file upload morgan multer lodash axios
+# npm install express pg bcrypt cors dotenv jsonwebtoken moment body-parser aws-sdk express-file upload morgan multer lodash axios twilio
 
 #run backend
 # npm run server
