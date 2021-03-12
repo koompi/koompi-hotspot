6faf0e3cdfa765de1c50bf03f5bb6abaea9745c2
LoebKalin <loeb.kalin79@gmail.com>
2021-03-12 17:53:53 +0700

    update

--- server/utils/check_validwallet.js
+++ server/utils/check_validwallet.js
@@ -1,58 +1,18 @@
-const axios = require("axios");
 require("dotenv").config();
-const pool = require("../../db");
 
 const { hexToU8a, isHex } = require("@polkadot/util");
 const { decodeAddress, encodeAddress } = require("@polkadot/keyring");
 
-
-
-const checkBalance = (usr,res,req)=>{
-    const balance;
-    try{
-    const checkWallet = await pool.query(
-        "SELECT ids FROM useraccount WHERE id = $1",
-        [usr]
+const isValidAddressPolkadotAddress = address => {
+  try {
+    encodeAddress(
+      isHex(address) ? hexToU8a(address) : decodeAddress(address, false, 42)
     );
 
-    const userPortfolio = {
-        id: checkWallet.rows[0].ids,
-        apikey: process.env.API_KEYs,
-        apisec: process.env.API_SEC,
-    };
-    if (checkWallet.rows[0].ids === null) {
-        res.status(401).json({message:"Please get wallet first!"});
-    } else {
-        axios
-            .post(
-                "https://testnet-api.selendra.com/apis/v1/portforlio-by-api",
-                userPortfolio
-            )
-            .then((r) => {
-                balance=JSON.parse(JSON.stringify(r.data.body.data.balance));
-            })
-            .catch((err) => {
-                console.error(err);
-            });
-    }
-} catch (err) {
-    console.error(err);
-    res.status(500).json({message:"Server error!"});
-}
-}
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
 
-
-const isValidAddressPolkadotAddress = address => {
-    try {
-      encodeAddress(
-        isHex(address) ? hexToU8a(address) : decodeAddress(address, false, 42)
-      );
-  
-      return true;
-    } catch (error) {
-      return false;
-    }
-  };
-  
-
-module.exports={checkBalance,isValidAddressPolkadotAddress};
+module.exports = { checkBalance, isValidAddressPolkadotAddress };
