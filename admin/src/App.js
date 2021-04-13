c74d05533bbf41cb6fa45f17e6f8b19fae19a6f3
LoebKalin <loeb.kalin79@gmail.com>
2021-04-13 16:17:31 +0700

    restructure admin

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
+// import Navbar from "./admin/components/layout/navbar/Navbar";
+// import Main from "./admin/components/pages/main/Main";
+// import Sidebar from "./admin/components/layout/sidebar/Sidebar";
+import Dashboard from "./components/dashboard";
+import ConfirmAdmin from "./components/email-verification";
+import Login from "./components/login";
+import PrivateRoute from "./PrivateRoute";
+import PublicRoute from "./PublicRoute";
+
+import "./App.css";
+import "./assets/css/App.css";
+
+const AppDashboard = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Router>
+        <Switch>
+          <Route exact path="/admin/" restrict={true} component={Login} />
+          <PublicRoute
+            exact
+            path="/admin/login"
+            restrict={true}
+            component={Login}
+          />
+          <PrivateRoute
+            exact
+            path="/admin/login/confirm-Admin"
+            component={ConfirmAdmin}
+          />
+          <PrivateRoute exact path="/admin/home" component={Dashboard} />
+        </Switch>
+      </Router>
+    </React.Fragment>
   );
-}
+};
 
-export default App;
+export default AppDashboard;
