40bb671e8b6e64caa967cbe63c37a010f944b017
Heng Hongsea <hongsea119@gmail.com>
2021-04-23 15:30:53 +0700

    add docker to server

---
+++ server/dev.sh
@@ -0,0 +1,7 @@
+cd server && npm install && npm run server
+
+#install node module
+# npm install express pg bcrypt cors dotenv jsonwebtoken moment body-parser aws-sdk express-file upload morgan multer lodash axios twilio
+
+#run backend
+# npm run server 
