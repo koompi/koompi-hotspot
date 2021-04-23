931ebaf97bc92a885dc4441b8ea857a04bfc325c
Heng Hongsea <hongsea119@gmail.com>
2021-04-23 15:41:06 +0700

    update

--- server/start.sh
+++ server/start.sh
@@ -1,4 +1,4 @@
-cd server && npm install && npm run server
+npm install && npm run server
 
 #install node module
 # npm install express pg bcrypt cors dotenv jsonwebtoken moment body-parser aws-sdk express-file upload morgan multer lodash axios twilio
