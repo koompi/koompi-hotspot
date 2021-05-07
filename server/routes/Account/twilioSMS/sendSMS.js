10e67ad27d1c1b77123caa64a381081ecc8f7222
LoebKalin <loeb.kalin79@gmail.com>
2021-05-07 08:42:10 +0700

    activate sendSMS method

--- server/routes/Account/twilioSMS/sendSMS.js
+++ server/routes/Account/twilioSMS/sendSMS.js
@@ -1,26 +1,48 @@
 require("dotenv").config();
 
+const qs = require("querystring");
+const http = require("https");
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const myPhone = process.env.TWILIO_PHONE_NUMBER;
-
-const client = require("twilio")(accountSid, authToken);
+const authToken = process.env.TWILIO_AUTH_Authorization;
 
 const sendSMS = async (to, message) => {
   try {
-    client.messages
-      .create({
-        body: message,
-        from: myPhone,
-        to: to,
-      })
-      .then((message) => {
-        console.log(message.sid);
-      })
-      .done();
+    const data = qs.stringify({
+      Body: message,
+      To: to,
+      From: "KOOMPI-FiFi"
+    });
+
+    const options = {
+      method: "POST",
+      hostname: "api.twilio.com",
+      port: null,
+      path: `/2010-04-01/Accounts/${accountSid}/Messages.json`,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Content-Length": data.length,
+        Authorization: authToken
+      }
+    };
+
+    const req = http.request(options, function(res) {
+      const chunks = [];
+
+      res.on("data", function(chunk) {
+        chunks.push(chunk);
+      });
+
+      res.on("end", function() {
+        const body = Buffer.concat(chunks);
+        console.log(body.toString());
+      });
+    });
+
+    req.write(data);
+    req.end();
   } catch (error) {
-    console.error(error.message);
+    console.log("Error at sendSMS method : ", error);
   }
 };
 module.exports = { sendSMS };
