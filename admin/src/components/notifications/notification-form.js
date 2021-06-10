80c439504a2dcf366999a3a280a975daf74a3aa5
LoebKalin <loeb.kalin79@gmail.com>
2021-06-11 01:42:25 +0700

    update notifi

--- admin/src/components/notifications/notification-form.js
+++ admin/src/components/notifications/notification-form.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
+import axios from 'axios'
 import { Form, Input, Button, Row, Col, Upload, message } from "antd";
 import { LoadingOutlined, PlusOutlined } from "@ant-design/icons";
 
 const { TextArea } = Input;
+const getToken = localStorage.getItem('token')
 
 function getBase64(img, callback) {
   const reader = new FileReader();
@@ -24,6 +26,39 @@ function beforeUpload(file) {
 
 const FormNotification = () => {
   const [state, setState] = useState(false);
+  const onSubmit = (value) => {
+    const auth = {
+      Authorization: "Bearer " + getToken,
+    };
+    // var bodyFormData = new FormData();
+    // bodyFormData.append('file', value.imageUrl);
+    // bodyFormData.append('title', value.title);
+    // bodyFormData.append('category', value.category);
+    // bodyFormData.append('description', value.desc);
+
+   const data={
+      file: imageUrl,
+      title : value.title,
+      category : value.category,
+      description : value.desc, 
+
+    };
+    // console.log(data);
+
+     axios({
+       method: "POST",
+       url:"http://localhost:5000/api/admin/notification",
+       data,
+       headers:{
+        "Content-Type": "multipart/form-data",
+          ...auth,
+       },
+     })
+       .then((res) => {
+        console.log("notification",res.data);        
+      })
+      .catch((err) => console.log(err));
+  };
 
   const handleChange = (info) => {
     if (info.file.status === "uploading") {
@@ -40,6 +75,16 @@ const FormNotification = () => {
       );
     }
   };
+  // const handleChange = (info)=> {
+  //   if (info.file.status !== 'uploading') {
+  //     console.log(info.file, info.fileList);
+  //   }
+  //   if (info.file.status === 'done') {
+  //     message.success(`${info.file.name} file uploaded successfully`);
+  //   } else if (info.file.status === 'error') {
+  //     message.error(`${info.file.name} file upload failed.`);
+  //   }
+  // };
 
   const { loading, imageUrl } = state;
   const uploadButton = (
@@ -52,7 +97,7 @@ const FormNotification = () => {
   return (
     <React.Fragment>
       <div className="contentContainer-auto">
-        <Form layout="vertical" size="large">
+        <Form layout="vertical" size="large" onFinish={onSubmit}>
           <Row gutter={[32, 32]}>
             <Col span={16}>
               <Row gutter={[32, 0]}>
@@ -87,9 +132,21 @@ const FormNotification = () => {
               </Row>
             </Col>
             <Col span={8}>
-              <Form.Item
+            <Form.Item
+                    label="Category"
+                    name="category"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input the message category!",
+                      },
+                    ]}
+                  >
+                  <TextArea rows={1} className="schoolInput" />
+            </Form.Item>
+            <Form.Item
                 label="Uplaod image here"
-                name="uplaod img"
+                name="upload_img"
                 rules={[
                   {
                     required: true,
@@ -103,7 +160,7 @@ const FormNotification = () => {
                   className="avatar-uploader"
                   showUploadList={false}
                   action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-                  beforeUpload={beforeUpload}
+                  // beforeUpload={beforeUpload}
                   onChange={handleChange}
                 >
                   {imageUrl ? (
@@ -119,8 +176,7 @@ const FormNotification = () => {
               </Form.Item>
             </Col>
           </Row>
-          <Button type="primary" className="publish-button">
-            {" "}
+          <Button type="primary" className="publish-button" htmlType="submit">
             Publish
           </Button>
         </Form>
@@ -129,4 +185,9 @@ const FormNotification = () => {
   );
 };
 
+
+
+
+
 export default FormNotification;
+
