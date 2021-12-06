9a561f15b7f879a7aea7bcad9f6a1394e500f889
sokunsamnang <sokunsamnang45@gmail.com>
2021-12-06 17:47:15 +0700

    config api alert noti

--- server/routes/Account/alertNotification.js
+++ server/routes/Account/alertNotification.js
@@ -6,39 +6,46 @@ const authorization = require("../../middleware/authorization");
 const OneSignal = require('onesignal-node');    
 
 
-router.put("/add-playerid", authorization, async (req, res) => {
+// create a new Client for a single app
+var myClient = new OneSignal.Client({
+    userAuthKey: `${process.env.API_USER_AUTH_KEY_ONESIGNAL}`,
+    // note that "app" must have "appAuthKey" and "appId" keys
+    app: { appAuthKey: `${process.env.API_KEY_ONESIGNAL}`, appId: `${process.env.API_ID_ONESIGNAL}` }
+});
+
+router.post("/alert-notification", authorization, async (req, res) => {
     try {
-      //1. destructure the req.body (full_name,gender , email, password,bithdate,address)
-  
-      const { player_id } = req.body;
-  
-      //2. check if user exist (if user exist then throw error)
-  
-      const user = await pool.query(
-        "SELECT * FROM useraccount WHERE id = $1",
-        [req.user]
-      );
-      if (user.rows.length === 0) {
-        return res.status(401).json({ message: "Account isn't exist yet!" });
-      }
-      const activate = await user.rows[0].activate;
-  
-      if (!activate) {
-        return res
-          .status(401)
-          .json({ message: "Please activate your account first!" });
-      } else {
-        await pool.query(
-          "UPDATE useraccount SET player_id=$1 WHERE id=$2",
-          [player_id, req.user]
+        //1. destructure the req.body (full_name,gender , email, password,bithdate,address)
+    
+        const { player_id } = req.body;
+    
+        //2. check if user exist (if user exist then throw error)
+    
+        const user = await pool.query(
+          "SELECT * FROM useraccount WHERE id = $1",
+          [req.user]
         );
-  
-        res.status(200).json({ message: "Completed Information Player ID." });
+        if (user.rows.length === 0) {
+          return res.status(401).json({ message: "Account isn't exist yet!" });
+        }
+        const activate = await user.rows[0].activate;
+    
+        if (!activate) {
+          return res
+            .status(401)
+            .json({ message: "Please activate your account first!" });
+        } else {
+          await pool.query(
+            "UPDATE useraccount SET player_id=$1 WHERE id=$2",
+            [player_id, req.user]
+          );
+    
+          res.status(200).json({ message: "Completed Information Player ID." });
+        }
+      } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ message: "Server Error!" });
       }
-    } catch (error) {
-      console.error(error.message);
-      res.status(500).json({ message: "Server Error!" });
-    }
-  });
+});
 
 module.exports = router;
