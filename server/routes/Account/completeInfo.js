b0e1e41dbc4afe48910f5682aa48d91854e14dbb
sokunsamnang <sokunsamnang45@gmail.com>
2021-12-06 15:21:03 +0700

    update id alert noti

--- server/routes/Account/completeInfo.js
+++ server/routes/Account/completeInfo.js
@@ -69,4 +69,39 @@ router.put("/complete-info", async (req, res) => {
     res.status(500).json({ message: "Server Error!" });
   }
 });
+
+router.put("/add-playerid", async (req, res) => {
+  try {
+    //1. destructure the req.body (full_name,gender , email, password,bithdate,address)
+
+    const { player_id } = req.body;
+
+    //2. check if user exist (if user exist then throw error)
+
+    const user = await pool.query(
+      "SELECT * FROM useraccount WHERE phone = $1",
+      [phone]
+    );
+    if (user.rows.length === 0) {
+      return res.status(401).json({ message: "Account isn't exist yet!" });
+    }
+    const activate = await user.rows[0].activate;
+
+    if (!activate) {
+      return res
+        .status(401)
+        .json({ message: "Please activate your account first!" });
+    } else {
+      await pool.query(
+        "UPDATE useraccount SET player_id=$1 WHERE id=$2",
+        [player_id, req.user]
+      );
+
+      res.status(200).json({ message: "Completed Information." });
+    }
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ message: "Server Error!" });
+  }
+});
 module.exports = router;
